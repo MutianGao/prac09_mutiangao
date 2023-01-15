86a1fca7ab0cf7cb883df779de84bba547a7b249
mutian gao JCU <mutian.gao@my.jcu.edu.au>
2023-01-15 21:00:06 +0800

    update

--- prac_09/taxi_simulator.py
+++ prac_09/taxi_simulator.py
@@ -3,11 +3,6 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
-
-def get_valid_taxi(taxis):
-    pass
-
-
 def main():
     bill = 0.00
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
